481657a74e1e0a9291493e7ea6f8e9769fdf15f4
zlhunzzing <zlhunzzing@gmail.com>
2020-10-13 13:40:55 +0900

    네비게이션 바 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './style.css';
 
 /* pages */
 import Home from './pages/Home';
+import NavigationComponent from './component/NavigationComponent';
 
 function App() {
+  const isAdmin = useState(true);
+
   return (
-    <div className="App">
+    <div className="App" style={isAdmin ? { paddingTop: '30px' } : {}}>
+      <NavigationComponent></NavigationComponent>
       <Switch>
         <Route path="/" render={() => <Home></Home>}></Route>
       </Switch>
---
+++ client/src/component/NavigationComponent.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function NavigationComponent() {
+  return (
+    <nav>
+      <ul className="nav-container">
+        <li className="nav-item">
+          <a href="1">글 작성</a>
+        </li>
+        {/* <li className="nav-item">
+          <a href="2">2</a>
+        </li>
+        <li className="nav-item">
+          <a href="3">3</a>
+        </li> */}
+      </ul>
+    </nav>
+  );
+}
+
+export default NavigationComponent;
