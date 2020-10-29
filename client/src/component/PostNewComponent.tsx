21b4c38d4f9ef4ca51cb13d5c6f15406be639c8f
zlhunzzing <zlhunzzing@gmail.com>
2020-10-29 14:25:46 +0900

    글 작성 폼 수정

--- client/src/component/PostNewComponent.tsx
+++ client/src/component/PostNewComponent.tsx
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 function PostNewComponent() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const category = useSelector((state: any) => state.Info.category);
+
+  useEffect(() => {
+    if((document.getElementsByName("category")[0] as any)) {
+      (document.getElementsByName("category")[0] as any).checked = true
+    }
+  })
 
   return (
-    <div
-      style={{
-        width: '70%',
-        height: '700px',
-        margin: '0 auto',
-        textAlign: 'left',
-      }}
-    >
-      <form>
-        <button
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            console.log(title, content);
-          }}
-        >
-          작성
-        </button>
+    <form style={{ margin: '10px' }}>
+      <span
+        onSubmit={(e) => {
+          e.preventDefault()
+          console.log("읭")
+        }}
+
+        style={{
+          display: 'inline-block',
+          textAlign: 'left',
+          width: '70%',
+          minHeight: '700px',
+        }}
+      >
         <textarea
           placeholder="제목"
           onChange={({ target: { value } }) => setTitle(value)}
           style={{
-            width: '100%',
+            width: '95%',
             height: '55px',
             fontSize: '30px',
             border: 'none',
@@ -48,14 +52,61 @@ function PostNewComponent() {
           placeholder="본문"
           onChange={({ target: { value } }) => setContent(value)}
           style={{
-            width: '100%',
-            height: '550px',
+            width: '95%',
+            height: '500px',
             border: 'none',
             resize: 'none',
           }}
         ></textarea>
-      </form>
-    </div>
+      </span>
+      <span
+        style={{
+          float: 'right',
+          textAlign: 'left',
+          width: '25%',
+          minHeight: '700px',
+          borderLeft: '1px solid rgb(230, 230, 230)',
+        }}
+      >
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            console.log(title, content);
+          }}
+          style={{ margin: '5px '}}
+        >
+          작성
+        </button>
+        <div
+          style={{
+            width: '100%',
+            borderTop: '1px solid rgb(230, 230, 230)',
+          }}
+        ></div>
+        <div>
+          <h5 style={{ margin: '10px' }}>카테고리</h5>
+          <ul style={{
+            marginTop: '0px',
+            listStyleType: 'none',
+            paddingLeft: '5px'
+          }}>
+            {category.map((list: any, id: number) => (
+              <li
+                key={id}
+                style={{
+                  display: 'block',
+                  margin: '5px'
+                }}
+              >
+                <input type="radio" name="category" value={`${list.id}`}></input>
+                  {' '}<label>{list.name}</label>
+                </li>
+              ))}
+            </ul>
+          </div>
+      </span>
+    </form>
   );
 }
 
